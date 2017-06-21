6773aed9515bf50dedca9cb6665d3bfa152a8841
30310배주애\dsm2015 <qowndo@gmail.com>
2017-06-21 11:07:45 +0900

    Upload python socket client(initializing commit)
    it is simple, same as client.c
    need supplement

---
+++ socket_client.py
@@ -0,0 +1,12 @@
+import socket
+
+HOST= '192.168.1.1'
+PORT = 1234
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+while 1 :
+    a = input()
+    s.send(ba)
+    data = s.recv(1024)
+s.close()
+print('Received', repr(data))
